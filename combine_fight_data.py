d23e522b888de9ffa596ebe6fea9c16450e8342f
joel_kennerley <joel_kennerley@hotmail.com>
2025-07-09 11:37:22 +1200

    exporting csv of combined data

--- combine_fight_data.py
+++ combine_fight_data.py
@@ -12,3 +12,4 @@ renamed1 = f1_features.rename(columns=lambda x: f'f1_{x}' if x in fighter_df els
 f1_f2_features = pd.merge(renamed1, fighter_df, how="left", left_on = 'fighter2_id', right_on='id')
 full_features = f1_f2_features.rename(columns=lambda x: f'f2_{x}' if x in fighter_df else x)
 
+full_features.to_csv('raw_features.csv', index=False)
